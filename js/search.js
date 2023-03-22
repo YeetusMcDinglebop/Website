f63d6867455906b09d411b02f67e78f61f067c01
Max Watson <107423821+YeetusMcDinglebop@users.noreply.github.com>
2023-03-22 02:13:28 -0700

    First Commit

---
+++ js/search.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search')
+search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const
+btn.addEventListener('click', () => {
+    search.classList.tottle('active')
+    input.focus()
+}}
